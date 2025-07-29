8f948f4007917a7ea8d7f26c97b3cca95e00da09
SonuTheCode <sy2440052@gmail.com>
2025-07-29 22:11:50 +0530

    Update if_else.c

--- if_ELSE_PROBLUM/if_else.c
+++ if_ELSE_PROBLUM/if_else.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+        int age ;
+        printf("Enter Your Valid Age\n");
+        scanf("%d", &age);
+        if(age>18){
+            printf("You Can Drive A Car\n");
+        }else if(age<18){
+            printf("You Can Not Drive Becuase You are kid \n");
+        }else{
+            printf("You Can Drive If You Have A Driving Lincance\n");
+        }
+        return 0 ;
+}
