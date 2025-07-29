5d31d84307ea1c6eeee361f492e496b6ca5bf7be
SonuTheCode <sy2440052@gmail.com>
2025-07-29 22:05:53 +0530

    Update 01_file.c

--- File_Input_Output/01_file.c
+++ File_Input_Output/01_file.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *ptr;
+    ptr = fopen("sonu.txt","r");
+    if (ptr == NULL)
+    {
+        printf("The file is not exist !");
+    }
+    int num;
+    fscanf(ptr, "%d", &num);
+    printf("the value of num is %d \n", num);
+    fscanf(ptr, "%d", &num);
+    printf("the value of num is %d \n", num);
+
+    fclose(ptr);
+    return 0;
+}
