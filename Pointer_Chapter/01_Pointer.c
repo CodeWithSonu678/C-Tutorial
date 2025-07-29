72367768a425af05621402d97bf9db329eda6312
SonuTheCode <sy2440052@gmail.com>
2025-07-29 22:09:45 +0530

    Update 01_Pointer.c

--- Pointer_Chapter/01_Pointer.c
+++ Pointer_Chapter/01_Pointer.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    
+    printf("Enter the vale of a is :");
+    scanf(" %d", &a);
+    int *p = &a;
+    int *p1 = &p;
+   
+    printf("The value of a is :%d\n", a);
+    printf("The address of a is :%u\n", p);
+    printf("The address value is :%d\n", *p);
+    printf("The address value is :%u\n", p1);
+    
+    
+    return 0;
+}
