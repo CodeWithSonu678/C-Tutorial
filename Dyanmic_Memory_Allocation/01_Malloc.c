ec786893794f2314ee6b252057f64a35994a7d8b
SonuTheCode <sy2440052@gmail.com>
2025-07-29 20:09:36 +0530

    Update 01_Malloc.c

--- Dyanmic_Memory_Allocation/01_Malloc.c
+++ Dyanmic_Memory_Allocation/01_Malloc.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n;
+    printf("Who many number you want :");
+    scanf("%d", &n);
+
+    int *m = (int *)malloc(n * sizeof(int));
+    int *ca = (int *)calloc(n, sizeof(int));
+
+    if (m == NULL || ca == NULL)
+    {
+        printf("Memory allocation is failed !");
+        return 1;
+    }
+
+    printf("\n malloc mein value pass karna \n");
+    scanf("");
+
+    return 0;
+}
