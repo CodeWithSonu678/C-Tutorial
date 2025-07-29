6895ad70dfabca4cb040ca3a22425c089df18923
SonuTheCode <sy2440052@gmail.com>
2025-07-29 22:10:46 +0530

    Update 01_fgets.c

--- String_Chapter/01_fgets.c
+++ String_Chapter/01_fgets.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char st[30];
+    printf("Enter the strings :");
+    fgets(st, sizeof(st), stdin);
+    printf("Entered the strings is %s", st);
+
+    return 0;
+}
 
