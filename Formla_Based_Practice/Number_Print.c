fea78d6ad24ae4b82c48cee4826b3588229f01d4
SonuTheCode <sy2440052@gmail.com>
2025-07-29 22:07:12 +0530

    Update Number_Print.c

--- Formla_Based_Practice/Number_Print.c
+++ Formla_Based_Practice/Number_Print.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Simple for loop
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i); // Print the current number
+    }
+    
+    return 0;
+}
 
