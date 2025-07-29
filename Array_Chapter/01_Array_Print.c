e31d35642e81c1d49a5fd54b1f6c23468f042677
SonuTheCode <sy2440052@gmail.com>
2025-07-29 20:04:55 +0530

    Update 01_Array_Print.c

--- Array_Chapter/01_Array_Print.c
+++ Array_Chapter/01_Array_Print.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int arr[5]= {50,34,55,33,44};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("The index of arr %d and this value is %d\n",i,arr[i]);
+    }
+    
+
+    return 0;
+}
